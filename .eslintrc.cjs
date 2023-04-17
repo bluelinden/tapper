c561f1c066f3a2f3356165a55fe2e11692208184
blue linden <dev@bluelinden.art>
2023-04-17 09:24:21 -0400

    use google code style for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,7 @@ module.exports = {
     'es2021': true
   },
   'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    './node_modules/gts'
   ],
   'overrides': [
   ],
